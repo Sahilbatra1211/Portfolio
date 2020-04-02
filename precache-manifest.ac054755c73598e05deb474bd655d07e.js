8955640d4a309c5c3e52bdbee68929c798c65d0a
sahilbatra1211 <sahilbatra1211@gmail.com>
2020-04-03 00:36:56 +0530

    Updates

---
+++ precache-manifest.ac054755c73598e05deb474bd655d07e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476233271a1d9ce53ea224d790ff6640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0f488d2a8f3583471de",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e1e5cbc0944e5e9867d3",
+    "url": "/static/css/main.271aa8a4.chunk.css"
+  },
+  {
+    "revision": "e0f488d2a8f3583471de",
+    "url": "/static/js/2.81bc9f32.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.81bc9f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e5cbc0944e5e9867d3",
+    "url": "/static/js/main.24e62a5e.chunk.js"
+  },
+  {
+    "revision": "fab8a60221159ed68a4b",
+    "url": "/static/js/runtime-main.3d39db7b.js"
+  },
+  {
+    "revision": "c8d76ea8de07105336abab4e1027338a",
+    "url": "/static/media/circle-cropped.c8d76ea8.png"
+  }
+]);
